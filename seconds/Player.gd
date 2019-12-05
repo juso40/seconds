9d672d8313210a0e5a04e08c29b4f63f60f15103
juso40 <justin.sostmann@googlemal.com>
2019-12-05 14:12:50 +0100

    first commit

---
+++ seconds/Player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+signal hit
+
+export var speed = 400  # pixel/sec
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):  # Jumping will be implemented later
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play("moving_right")
+		if velocity.x != 0:
+			$AnimatedSprite.flip_h = velocity.x < 0
+
+	else:
+		$AnimatedSprite.play("standing")
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
