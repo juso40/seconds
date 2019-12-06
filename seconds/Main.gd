059dbf35a38432353192aca9e92f3688e92d5665
juso40 <justin.sostmann@googlemal.com>
2019-12-06 19:22:54 +0100

    small progress on ui/scene switching

--- seconds/Main.gd
+++ seconds/Main.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-export (PackedScene) var Map
+const MAIN_MENU = "res://MainMenu.tscn"
 
+var current_map
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	self.show_map(MAIN_MENU)
+	# get_tree().change_scene("res://MainMenu.tscn")
 
-
-func _process(delta):
-	pass
+func show_map(map_name):
+	if self.current_map != null:
+		self.current_map.queue_free()
+	self.current_map = load(map_name).instance()
+	self.add_child(self.current_map)
